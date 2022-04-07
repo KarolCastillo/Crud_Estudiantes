9c0ad3c18f1b44d605d46bf9d01d9bb1ac572d55
karolcastillo <kcastillor10@miumg.edu.gt>
2022-04-07 16:03:08 -0500

    se implementan la alerta de SweetAlert al momento de eliminar en la vista que lista los grados

--- resources/views/Grado/listagrado.blade.php
+++ resources/views/Grado/listagrado.blade.php
@@ -1,76 +1,72 @@
 @extends('diseños.base')
 @section('content')
 
-<div class="container mt-4">
-    <div class="row justify-content-center">
-        <div class="col-md-15">
-            <h2 class="text-center mb-5"> LISTADO DE LOS GRADOS </h2>
-            <a type="button " href="{{ url('/CREAR_GRADO')}}" class="btn btn-success mb-3 mt-3 mr-2 md-3 offset float-left">NUEVO </a>
-            <table class="table table-bordered table-strpied text-center">
-                <thead class="thead-dark">
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">GRADO</th>
-                    <th scope="col">OPCIONES</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                    @foreach($grado as $grados)
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-15">
+                <h2 class="text-center mb-5"> LISTADO DE LOS GRADOS </h2>
+                <a type="button " href="{{ url('/CREAR_GRADO')}}" class="btn btn-success mb-3 mt-3 mr-2 md-3 offset float-left">NUEVO </a>
+                <table class="table table-bordered table-strpied text-center">
+                    <thead class="thead-dark">
                     <tr>
-
-                        <td class=" border px-4 py-2">{{$grados->id}}</td>
-                        <td class=" border px-4 py-2">{{$grados->descripcion}}</td>
-
-                        <td class=" border px-4 py-2">
-                            <div class="btn-group flex justify-center rounded-lg text-lg">
-                            <form action="{{ route('delete_grado',$grados->id)}}" class="eliminar-registro" method="POST">
-                                  @csrf @method('DELETE')
-                            <button type="submit" onclick="return confirm('Esta seguro de eliminar registro de grado');" class=" btn btn-danger" >
-                                <i class="fas fa-trash-alt"></i>
-                            </button>
-                            </form>
-                            </div>
-                        </td>
+                        <th scope="col">ID</th>
+                        <th scope="col">GRADO</th>
+                        <th scope="col">OPCIONES</th>
                     </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div
-            {{$grado->links()}}
+                    </thead>
 
+                    <tbody>
+                    @foreach($grado as $grados)
+                        <tr>
+
+                            <td class=" border px-4 py-2">{{$grados->id}}</td>
+                            <td class=" border px-4 py-2">{{$grados->descripcion}}</td>
+
+                            <td class=" border px-4 py-2">
+                                <div class="btn-group flex justify-center rounded-lg text-lg">
+                                    <form action="{{ route('delete_grado',$grados->id)}}" id="{{$grados->id}}" method="POST">
+                                        @csrf @method('DELETE')
+                                        <button type="button" onclick="eliminarGrado({{$grados->id}})" class="btn btn-danger" >
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <div
+                {{$grado->links()}}
+
+            </div>
         </div>
     </div>
-</div>
 @endsection
 
 <!--seccion de la alerta-->
 @section('alert')
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-    $('.eliminar-registro').submit(function(r){
-        r.preventDefault();
-
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
-                )
-            }
-        })
-
-    });
-
-
+        function eliminarGrado(grado){
+            Swal.fire({
+                title: 'Estas seguro de eliminar el grado?',
+                text: "No podras revertir estoa accion!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, Eliminar!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById(grado).submit()
+                    Swal.fire(
+                        'Eliminado!',
+                        'El grado desaparecio.',
+                        'success'
+                    )
+                }
+            })
+        }
     </script>
 @endsection
